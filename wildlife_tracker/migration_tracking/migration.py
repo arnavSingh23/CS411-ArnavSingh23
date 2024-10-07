07af62cdceb609494e26dd608b231fdf55127125
Arnav Singh <asingh23@bu.edu>
2024-10-07 10:04:52 -0400

    Fixed part 1 code

--- wildlife_tracker/migration_tracking/migration.py
+++ wildlife_tracker/migration_tracking/migration.py
@@ -1,5 +1,17 @@
-from typing import Any
+from typing import Optional
+from wildlife_tracker.habitat_management.habitat import Habitat
+from wildlife_tracker.migration_tracking.migration_path import MigrationPath 
 
 class Migration:
 
-    pass
+    def __init__(self, 
+                 migration_id: int,
+                 migration_path: MigrationPath,  
+                 current_location: str,
+                 status: str = "Scheduled",
+                 health_status: Optional[str] = None) -> None:
+        self.migration_id = migration_id
+        self.migration_path = migration_path
+        self.current_location = current_location
+        self.status = status
+        self.health_status = health_status
